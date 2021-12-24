3f42e91deed2cec30da5573f37d249f4aba6c224
ajandaur <anmol.jandaur@gmail.com>
2021-12-24 10:42:00 -0800

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Anmol  Jandaur on 12/24/21.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
